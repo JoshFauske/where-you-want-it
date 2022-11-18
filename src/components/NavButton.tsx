cddba4622b9431bf538696300f114e8296355626
Josh Fauske <jfauske11@gmail.com>
2022-11-18 16:28:50 -0700

    Added active css to nav buttons

--- src/components/NavButton.tsx
+++ src/components/NavButton.tsx
@@ -4,12 +4,13 @@ import { Link } from "react-router-dom";
 type NavButtonProps = {
   text: string;
   href: string;
+  active: boolean;
 };
 
-const NavButton = ({ href, text }: NavButtonProps) => {
+const NavButton = ({ href, text, active }: NavButtonProps) => {
   return (
     <Link to={href}>
-      <a className="ml-3">{text}</a>
+      <a className={`ml-3 ${active ? "font-bold border-b-2 border-primaryColor" : ""}`}>{text}</a>
     </Link>
   );
 };
--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,24 +1,40 @@
+import { useLocation } from "react-router-dom";
 import Logo from "./Logo";
 import NavButton from "./NavButton";
 
 type TopBarProps = {};
 
 const TopBar = ({}: TopBarProps) => {
-	return (
-		<div className="container mx-auto py-4">
-			<div className="flex items-center justify-between">
-				<div className="flex items-center">
-					<Logo width={56} className="mr-2" />
-					<h3>Where You Want It</h3>
-				</div>
-				<div className="flex items-center">
-					<NavButton text="Home" href="/" />
-					<NavButton text="Services" href="/services" />
-					<NavButton text="Gallery" href="/gallery" />
-				</div>
-			</div>
-		</div>
-	);
+  const location = useLocation();
+  const pathName = location.pathname;
+
+  return (
+    <div className="container mx-auto py-4">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center">
+          <Logo width={56} className="mr-2" />
+          <h3>Where You Want It</h3>
+        </div>
+        <div className="flex items-center">
+          <NavButton
+            text="Home"
+            href="/"
+            active={pathName == "/" ? true : false}
+          />
+          <NavButton
+            text="Services"
+            href="/services"
+            active={pathName == "/services" ? true : false}
+          />
+          <NavButton
+            text="Gallery"
+            href="/gallery"
+            active={pathName == "/gallery" ? true : false}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default TopBar;
