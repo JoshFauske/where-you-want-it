d14bcbb0e8d778e1cb055a944fff866929dcc374
Josh Fauske <jfauske11@gmail.com>
2022-11-18 06:18:08 -0700

    Added hero image to all pages

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import Hero from "../components/Hero";
 
 const Gallery = () => {
-	return <div>Gallery</div>;
+  return (
+    <div>
+      <Hero
+        title="Moving can be fun, if you let it be"
+        text="Check out some pictures of our previous moves and witness for yourself how easy it really is."
+        imageSrc="/src/assets/gallery.png"
+        imageAlt="Happy Employee"
+      />
+    </div>
+  );
 };
 
 export default Gallery;
--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import Hero from "../components/Hero";
 
 const Services = () => {
-	return <div>Services</div>;
+  return (
+    <div>
+      <Hero
+        title="From moving to storage, we do it all"
+        text="If you don’t want to worry how to move or store your things, you’ve come to the right place!"
+        imageSrc="/src/assets/services.png"
+        imageAlt="Moving Truck"
+      />
+    </div>
+  );
 };
 
 export default Services;
