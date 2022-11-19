6249d082528009b29076d2ffe14949c2f942733f
Josh Fauske <jfauske11@gmail.com>
2022-11-19 15:14:37 -0700

    Added spacing above hero image and quote form

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -1,15 +1,23 @@
 import React from "react";
 import Hero from "../components/Hero";
+import QuoteForm from "../components/QuoteForm";
+import Section from "../components/Section";
+import SectionTitle from "../components/SectionTitle";
 
 const Gallery = () => {
   return (
     <div>
+      <div className="sm:mb-2"></div>
       <Hero
         title="Moving can be fun, if you let it be"
         text="Check out some pictures of our previous moves and witness for yourself how easy it really is."
         imageSrc="/src/assets/gallery.png"
         imageAlt="Happy Employee"
       />
+      <Section>
+        <SectionTitle title="Get a Quote" />
+        <QuoteForm />
+      </Section>
     </div>
   );
 };
--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,15 +1,23 @@
 import React from "react";
 import Hero from "../components/Hero";
+import QuoteForm from "../components/QuoteForm";
+import Section from "../components/Section";
+import SectionTitle from "../components/SectionTitle";
 
 const Services = () => {
   return (
     <div>
+      <div className="sm:mb-3 lg:mb-8"></div>
       <Hero
         title="From moving to storage, we do it all"
         text="If you don’t want to worry how to move or store your things, you’ve come to the right place!"
         imageSrc="/src/assets/services.png"
         imageAlt="Moving Truck"
       />
+      <Section>
+        <SectionTitle title="Get a Quote" />
+        <QuoteForm />
+      </Section>
     </div>
   );
 };
