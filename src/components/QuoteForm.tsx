32b09820977f9f1c02611cb177ed511919d0a793
Josh Fauske <jfauske11@gmail.com>
2022-11-19 06:46:16 -0700

    Added section, sectionTitle, and quoteForm components

---
+++ src/components/QuoteForm.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type QuoteFormProps = {};
+
+const QuoteForm = ({}: QuoteFormProps) => {
+  return <div className="flex justify-center">
+
+	</div>;
+};
+
+export default QuoteForm;
