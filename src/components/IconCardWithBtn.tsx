3d7f1fee6882ce7a59b8ac3e83d2d66eca71e87e
Josh Fauske <jfauske11@gmail.com>
2022-11-20 06:30:41 -0700

    Icons for services page

--- src/components/IconCardWithBtn.tsx
+++ src/components/IconCardWithBtn.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { ReactNode } from "react";
 
-const IconCardWithBtn = () => {
-	return (
-		<div>IconCardWithBtn</div>
-	)
-}
+type IconCardWithBtnProps = {
+  icon: ReactNode;
+  text: string;
+};
 
-export default IconCardWithBtn
+const IconCardWithBtn = ({ icon, text }: IconCardWithBtnProps) => {
+  return (
+    <div className="border-2 border-primaryColor h-44">
+      <div className="flex flex-col justify-between p-2 sm:p-4 h-full">
+        <div className="uppercase text-center font-bold">{text}</div>
+        <i>{icon}</i>
+				<button className="text-white bg-black w-full">Read More</button>
+      </div>
+    </div>
+  );
+};
+
+export default IconCardWithBtn;
--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,5 +1,8 @@
 import React from "react";
+import { BsHouseDoor, BsBuilding } from "react-icons/bs";
+import { AiOutlineCar } from "react-icons/ai";
 import Hero from "../components/Hero";
+import IconCardWithBtn from "../components/IconCardWithBtn";
 import QuoteForm from "../components/QuoteForm";
 import Section from "../components/Section";
 import SectionTitle from "../components/SectionTitle";
@@ -14,6 +17,14 @@ const Services = () => {
         imageSrc="/src/assets/services.png"
         imageAlt="Moving Truck"
       />
+			<Section>
+				<SectionTitle title="Moving & Storage" />
+				<div className="grid grid-cols-1 sm:grid-cols-3 gap-4 w-full">
+					<IconCardWithBtn icon={<BsHouseDoor size={60} className="m-auto" />} text="Residential" />
+					<IconCardWithBtn icon={<BsBuilding size={60} className="m-auto" />} text="Commercial" />
+					<IconCardWithBtn icon={<AiOutlineCar size={60} className="m-auto" />} text="Vehicles" />
+				</div>
+			</Section>
       <Section>
         <SectionTitle title="Get a Quote" />
         <QuoteForm />
