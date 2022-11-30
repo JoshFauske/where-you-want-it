510364bfd520627a7766d53e4f5e74bff1c67b19
Jfauskee <42004855+Jfauskee@users.noreply.github.com>
2022-11-30 08:04:37 -0700

    Changed mobile nav button color

--- src/components/MobileNavButton.tsx
+++ src/components/MobileNavButton.tsx
@@ -2,36 +2,36 @@ import React, { ReactNode } from "react";
 import { Link } from "react-router-dom";
 
 type MobileNavButtonProps = {
-  text: string;
-  href: string;
-  active: boolean;
-  onClick: any;
-  icon: ReactNode;
+	text: string;
+	href: string;
+	active: boolean;
+	onClick: any;
+	icon: ReactNode;
 };
 
 const MobileNavButton = ({
-  href,
-  text,
-  active,
-  onClick,
-  icon,
+	href,
+	text,
+	active,
+	onClick,
+	icon,
 }: MobileNavButtonProps) => {
-  return (
-    <Link to={href}>
-      <div
-        className={`w-fit mb-3 flex items-center underline font-bold ${
-          active ? "text-primaryColor !border-primaryColor" : ""
-        }`}
-        onClick={onClick}
-        onClickCapture={() => {
-          window.scrollTo(0, 0);
-        }}
-      >
-        <span className="mr-2">{icon}</span>
-        <span>{text}</span>
-      </div>
-    </Link>
-  );
+	return (
+		<Link to={href}>
+			<div
+				className={`w-fit mb-3 flex items-center underline font-bold ${
+					active ? "text-white" : ""
+				}`}
+				onClick={onClick}
+				onClickCapture={() => {
+					window.scrollTo(0, 0);
+				}}
+			>
+				<span className="mr-2">{icon}</span>
+				<span>{text}</span>
+			</div>
+		</Link>
+	);
 };
 
 export default MobileNavButton;
